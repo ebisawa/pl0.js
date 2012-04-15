c413b6d9adc51df29340ab80c6d56791e4e1f9cb
Satoshi Ebisawa <ebisawa@gmail.com>
2012-04-15 20:01:05 +0900

    wip

--- plz.js
+++ plz.js
@@ -23,7 +23,7 @@ function PLZ() {
             var tokens = [];
             var unget_token;
 
-            var REGEXP = /\s+|[a-zA-Z][a-zA-Z0-9]*|[0-9]+|:=|<=|>=|[+\-*=#<>(),:;!\.]/;
+            var REGEXP = /\s+|[a-zA-Z][a-zA-Z0-9]*|[0-9]+|:=|<=|>=|[+\-*\/=#<>(),:;!\.]/;
             var KEYWORDS = [
                 "begin", "call", "const", "do", "end", "if",
                 "odd", "print", "procedure", "then", "var", "while",
@@ -79,7 +79,7 @@ function PLZ() {
             };
         }
 
-        function Parser(lexer) {
+        function Parser(lexer, debug) {
             function NodeBlock(consts, vars, procs, stmts) {
                 return { ntype: "block", consts: consts, vars: vars, procedures: procs, statements: stmts };
             }
@@ -245,6 +245,9 @@ function PLZ() {
             function program() {
                 var r;
 
+                if (debug)
+                    console.log("-- parser: program --");
+
                 r = block();
                 check_next_keyword(".");
 
@@ -256,6 +259,9 @@ function PLZ() {
             //         { "procedure" ident ";" block ";" } statement .
             function block() {
                 var token, c, v, p, s;
+
+                if (debug)
+                    console.log("-- parser: block --");
 
                 token = lexer.getnext();
                 c = []; v = []; p = [];
@@ -290,6 +296,9 @@ function PLZ() {
             function block_var() {
                 var token, vars = [];
 
+                if (debug)
+                    console.log("-- parser: block_var --");
+
                 for (;;) {
                     token = lexer.getnext();
                     if (token.type != IDENT)
@@ -311,6 +320,9 @@ function PLZ() {
             function block_procedure() {
                 var token, n, b;
 
+                if (debug)
+                    console.log("-- parser: block_procedure --");
+
                 token = lexer.getnext();
                 if (token.type != IDENT)
                     error_unexpected(token);
@@ -329,6 +341,9 @@ function PLZ() {
             //             "while" condition "do" statement ].
             function statement() {
                 var token = lexer.getnext();
+
+                if (debug)
+                    console.log("-- parser: statement --");
 
                 if (token.is_keyword("begin"))
                     return statement_begin();
@@ -419,6 +434,9 @@ function PLZ() {
             function condition() {
                 var v1, v2, token;
 
+                if (debug)
+                    console.log("-- parser: condition --");
+
                 token = lexer.getnext();
                 if (token.is_keyword("odd")) {
                     v1 = expression();
@@ -444,6 +462,9 @@ function PLZ() {
             // expression = ["+"|"-"] term { ("+"|"-") term}.
             function expression() {
                 var v1, v2, node, token;
+
+                if (debug)
+                    console.log("-- parser: expression --");
 
                 /* XXX */
                 token = lexer.getnext();
@@ -473,6 +494,9 @@ function PLZ() {
             function term() {
                 var v1, v2, node, token;
 
+                if (debug)
+                    console.log("-- parser: term --");
+
                 v1 = factor();
 
                 for (;;) {
@@ -490,6 +514,9 @@ function PLZ() {
             // factor = ident | number | "(" expression ")".
             function factor() {
                 var r;
+
+                if (debug)
+                    console.log("-- parser: factor --");
 
                 token = lexer.getnext();
                 if (token.type == IDENT)
@@ -535,9 +562,8 @@ function PLZ() {
 
         return {
             parse: function(source) {
-                var lexer;
+                var lexer = Lexer(source);
 
-                lexer = Lexer(source);
                 try {
                     return Parser(lexer).parse();
                 } catch (e) {
@@ -552,21 +578,20 @@ function PLZ() {
         var gregs = 0;
 
         function Environ(name, parent, vars, global) {
-            var i, regname, regs = gregs;
-
             this.name = name;
             this.parent = parent;
+            this.regs = gregs;
             this.vars = {};
             this.global = global;
             this.label_index = 0;
 
-            for (i = 0; i < vars.length; i++) {
-                this.vars[vars[i]] = "r" + regs;
-                regs += 1;
+            for (var i = 0; i < vars.length; i++) {
+                this.vars[vars[i]] = "r" + this.regs;
+                this.regs += 1;
             }
 
             if (global)
-                gregs = regs;
+                gregs = this.regs;
         }
 
         Environ.prototype.getreg = function(value) {
@@ -577,6 +602,10 @@ function PLZ() {
                 return this.parent.getreg(value);
             else
                 return reg;
+        };
+
+        Environ.prototype.gettmpreg = function() {
+            return "r" + this.regs++;
         };
 
         Environ.prototype.getlocalregs = function() {
@@ -605,7 +634,7 @@ function PLZ() {
             console.log("---- environ ----");
             console.log(env);
 
-            rcode = rcode.concat(codegen_statements(env, block.statements));
+            rcode = merge_code(rcode, codegen_statements(env, block.statements));
             rcode.push([ "ret" ]);
 
             return rcode;
@@ -621,7 +650,7 @@ function PLZ() {
                 console.log(r);
 
                 if (typeof r[0] == "object")
-                    rcode = rcode.concat(r);
+                    rcode = merge_code(rcode, r);
                 else
                     rcode.push(r);
             }
@@ -669,42 +698,68 @@ function PLZ() {
         }
 
         function codegen_stmt_op(env, stmt, dst) {
-            var v1, v2, vdst;
+            var v1, v2, vdst, rcode = [];
+
+            console.log("---- statement: operation ----");
+            console.log(stmt);
 
             function stmt_value(value) {
+                var rtmp;
+
                 switch (value.ntype) {
                 case "number":
                     return value.value;
                 case "ident":
                     return env.getreg(value.value);
+                case "operation":
+                    rtmp = env.gettmpreg();
+                    rcode = merge_code(rcode, codegen_stmt_op(env, value, rtmp));
+                    return rtmp;
+
                 default:
                     throw "unknown value type";
                 }
             }
 
-            v1 = stmt_value(stmt.value1);
-            v2 = stmt_value(stmt.value2);
+            v1 = stmt_value(stmt.value1, rcode);
+            v2 = stmt_value(stmt.value2, rcode);
             vdst = (dst == undefined) ? v1 : dst;
 
             switch (stmt.opcode) {
             case "+":
-                return [ "add", vdst, v1, v2 ];
+                rcode.push([ "add", vdst, v1, v2 ]);
+                break;
             case "-":
-                return [ "sub", vdst, v1, v2 ];
+                rcode.push([ "sub", vdst, v1, v2 ]);
+                break;
             case "*":
-                return [ "mul", vdst, v1, v2 ];
+                rcode.push([ "mul", vdst, v1, v2 ]);
+                break;
             case "/":
-                return [ "div", vdst, v1, v2 ];
+                rcode.push([ "div", vdst, v1, v2 ]);
+                break;
+
             case "=":
-                return [ "bne", vdst, v1, v2 ];
+                rcode.push([ "bne", vdst, v1, v2 ]);
+                break;
             case "<":
-                return [ "bge", vdst, v1, v2 ];
+                rcode.push([ "bge", vdst, v1, v2 ]);
+                break;
+            case "<=":
+                rcode.push([ "bgt", vdst, v1, v2 ]);
+                break;
             case ">":
-                return [ "ble", vdst, v1, v2 ];
+                rcode.push([ "ble", vdst, v1, v2 ]);
+                break;
+            case ">=":
+                rcode.push([ "blt", vdst, v1, v2 ]);
+                break;
 
             default:
                 throw "unknown opcode";
             }
+
+            return rcode;
         }
 
         function codegen_stmt_if(env, stmt) {
@@ -714,8 +769,8 @@ function PLZ() {
             console.log(stmt);
 
             else_label = env.genlabel();
-            rcode.push(codegen_stmt(env, stmt.condition, else_label ));
-            rcode = rcode.concat(codegen_statements(env, stmt.statements));
+            rcode = merge_code(rcode, codegen_stmt(env, stmt.condition, else_label ));
+            rcode = merge_code(rcode, codegen_statements(env, stmt.statements));
             rcode.push([ "label", else_label ]);
 
             return rcode;
@@ -730,11 +785,11 @@ function PLZ() {
             end_label = env.genlabel();
 
             rcode.push([ "label", start_label ]);
-            rcode.push(codegen_stmt(env, stmt.condition, end_label ));
+            rcode = merge_code(rcode, codegen_stmt(env, stmt.condition, end_label ));
 
             console.log("-------- loop: statements --------");
 
-            rcode = rcode.concat(codegen_statements(env, stmt.statements));
+            rcode = merge_code(rcode, codegen_statements(env, stmt.statements));
             rcode.push([ "jmp", start_label ]);
             rcode.push([ "label", end_label ]);
 
@@ -762,6 +817,15 @@ function PLZ() {
 
         function codegen_stmt_print(env, stmt) {
             return [ "print", env.getreg(stmt.value) ];
+        }
+
+        function merge_code(basecode, newcode) {
+            if (typeof newcode[0] == "object")
+                return basecode.concat(newcode);
+            else {
+                basecode.push(newcode);
+                return basecode;
+            }
         }
 
         function print_code(code) {
@@ -924,7 +988,7 @@ function PLZ_ILVM(code, debug) {
                 writeback(dst, opr1 * opr2);
                 break;
             case "div":
-                writeback(dst, opr1 / opr2);
+                writeback(dst, parseInt(opr1 / opr2));
                 break;
 
             case "bne":
@@ -935,8 +999,16 @@ function PLZ_ILVM(code, debug) {
                 if (opr1 >= opr2)
                     pc = labelmap[dst];
                 break;
+            case "bgt":
+                if (opr1 > opr2)
+                    pc = labelmap[dst];
+                break;
             case "ble":
                 if (opr1 <= opr2)
+                    pc = labelmap[dst];
+                break;
+            case "blt":
+                if (opr1 < opr2)
                     pc = labelmap[dst];
                 break;
             case "jmp":
